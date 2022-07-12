afd48de5c4b5e8709ffef86b8347601426242f67
nachitoloquein <elnachoescachondo@gmail.com>
2022-07-12 02:20:29 -0400

    final

--- Australia/urls.py
+++ Australia/urls.py
@@ -13,9 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-
 from django.contrib import admin
 from django.urls import path
 #from django.conf.urls import url
@@ -24,4 +21,4 @@ from core import views
 urlpatterns = [
     path('', views.index, name="test"),
     path('predict', views.predict, name="predict")
-] + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+]
