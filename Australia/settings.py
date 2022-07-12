ebbd5650b339cdabb1f6bdf58f871953ff81d7d2
nachitoloquein <elnachoescachondo@gmail.com>
2022-07-12 02:06:28 -0400

    deployment

--- Australia/settings.py
+++ Australia/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
